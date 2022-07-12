446ea936efec8dc1cc2a5f39310b374c894331bd
Jason <git-admin@casjaysdev.com>
2022-07-11 21:09:46 -0400

    🗃️  modified: bin/entrypoint-apache.sh 🗃️

--- bin/entrypoint-apache.sh
+++ bin/entrypoint-apache.sh
@@ -1,101 +1,113 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version       : 202202020147-git
-# @Author        : Jason Hempstead
-# @Contact       : jason@casjaysdev.com
-# @License       : WTFPL
-# @ReadME        : docker-entrypoint --help
-# @Copyright     : Copyright: (c) 2022 Jason Hempstead, Casjays Developments
-# @Created       : Wednesday, Feb 02, 2022 01:47 EST
-# @File          : docker-entrypoint
-# @Description   :
-# @TODO          :
-# @Other         :
-# @Resource      :
+##@Version           :  202207112032-git
+# @Author            :  Jason Hempstead
+# @Contact           :  jason@casjaysdev.com
+# @License           :  LICENSE.md
+# @ReadME            :  entrypoint-apache.sh --help
+# @Copyright         :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @Created           :  Monday, Jul 11, 2022 20:32 EDT
+# @File              :  entrypoint-apache.sh
+# @Description       :  
+# @TODO              :  
+# @Other             :  
+# @Resource          :  
+# @sudo/root         :  no
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="$(basename "$0")"
-VERSION="202202020147-git"
-USER="${SUDO_USER:-${USER}}"
-HOME="${USER_HOME:-${HOME}}"
+APPNAME="$(basename "$0" 2>/dev/null)"
+VERSION="202207112032-git"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
 if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
-trap 'exitCode=${exitCode:-$?};[ -n "$DOCKER_ENTRYPOINT_TEMP_FILE" ] && [ -f "$DOCKER_ENTRYPOINT_TEMP_FILE" ] && rm -Rf "$DOCKER_ENTRYPOINT_TEMP_FILE" &>/dev/null' EXIT
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-SSL="${SSL:-}"
-HOSTADMIN="${HOSTADMIN:-admin@localhost}"
-export TZ="${TZ:-America/New_York}"
-export HOSTNAME="${HOSTNAME:-casjaysdev-apache}"
+# Set functions
+__exec_bash() {
+  local cmd="${*:-/bin/bash}"
+  local exitCode=0
+  echo "running command: $cmd"
+  $cmd || exitCode=10
+  return ${exitCode:-$?}
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__find() { ls -A "$*" 2>/dev/null; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Define default variables
+TZ="${TZ:-America/New_York}"
+HOSTNAME="${HOSTNAME:-casjaysdev-bin}"
+BIN_DIR="${BIN_DIR:-/usr/local/bin}"
+DATA_DIR="${DATA_DIR:-$(__find /data/ 2>/dev/null | grep '^' || false)}"
+CONFIG_DIR="${CONFIG_DIR:-$(__find /config/ 2>/dev/null | grep '^' || false)}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional variables
 
-[ -n "${TZ}" ] && echo "${TZ}" >/etc/timezone
-[ -n "${HOSTNAME}" ] && echo "${HOSTNAME}" >/etc/hostname
-[ -n "${HOSTNAME}" ] && echo "127.0.0.1 $HOSTNAME localhost" >/etc/hosts
-[ -f "/usr/share/zoneinfo/${TZ}" ] && ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
-
-if [ -f "/config/ssl/server.crt" ] && [ -f "/config/ssl/server.key" ]; then
-  SSL="on"
-  SSL_CERT="/config/ssl/server.crt"
-  SSL_KEY="/config/ssl/server.key"
-  if [ -f "/config/ssl/ca.crt" ]; then
-    cat "/config/ssl/ca.crt" >>"/etc/ssl/certs/ca-certificates.crt"
-  fi
-elif [ "$SSL" = "on" ]; then
-  create-ssl-cert "/config/ssl"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Export variables
+export TZ HOSTNAME
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set timezone
+[[ -n "${TZ}" ]] && echo "${TZ}" >/etc/timezone
+[[ -f "/usr/share/zoneinfo/${TZ}" ]] && ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set hostname
+if [[ -n "${HOSTNAME}" ]]; then
+  echo "${HOSTNAME}" >/etc/hostname
+  echo "127.0.0.1 ${HOSTNAME} localhost ${HOSTNAME}.local" >/etc/hosts
 fi
-update-ca-certificates
-
-if [ "$SSL" = on ] && [ -z "$CONFIG" ]; then
-  CONFIG="/config/apache2/httpd.ssl.conf"
-else
-  CONFIG="${APACHE_CONF:-/config/apache2/httpd.conf}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Delete any gitkeep files
+[[ -n "${CONFIG_DIR}" ]] && { [[ -d "${CONFIG_DIR}" ]] && rm -Rf "${CONFIG_DIR}/.gitkeep" || mkdir -p "/config/"; }
+[[ -n "${DATA_DIR}" ]] && { [[ -d "${DATA_DIR}" ]] && rm -Rf "${DATA_DIR}/.gitkeep" || mkdir -p "/data/"; }
+[[ -n "${BIN_DIR}" ]] && { [[ -d "${BIN_DIR}" ]] && rm -Rf "${BIN_DIR}/.gitkeep" || mkdir -p "/bin/"; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy config files to /etc
+if [[ -n "${CONFIG_DIR}" ]]; then
+  for config in ${CONFIG_DIR}; do
+    if [[ -d "/config/$config" ]]; then
+      [[ -d "/etc/$config" ]] || mkdir -p "/etc/$config"
+      cp -Rf "/config/$config/." "/etc/$config/"
+    elif [[ -f "/config/$config" ]]; then
+      cp -Rf "/config/$config" "/etc/$config"
+    fi
+  done
 fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional commands
 
-if [ -f "/config/php8/php.ini" ]; then
-  cp -Rf "/config/php8/php.ini" "/etc/php8/php.ini"
-else
-  mkdir -p "/config/php8"
-  cp -Rf "/etc/php8/php.ini" "/config/php8/php.ini"
-fi
-
-if [ -d "/config/php8/php-fpm.d" ]; then
-  cp -Rf /config/php8/php-fpm.* "/etc/php8/"
-else
-  mkdir -p "/config/php8"
-  cp -Rf /etc/php8/php-fpm.* "/config/php8/"
-fi
-
-if [ ! -f "/data/htdocs/www/index.html" ] || [ ! -f "/data/htdocs/www/index.php" ]; then
-  [ -f "/data/htdocs/.docker_complete" ] || cp -Rf "/var/www/localhost/htdocs/." "/data/htdocs/www/"
-  touch "/data/htdocs/.docker_complete"
-fi
-
-if [ -f "/etc/apache2/httpd.conf" ] && [ ! -f "$CONFIG" ]; then
-  cp -Rf "/etc/apache2/httpd.conf" "$CONFIG"
-  sed -i "s/ServerName .*/ServerName $HOSTNAME/" "$CONFIG"
-  sed -i "s/ServerAdmin .*/ServerAdmin $HOSTADMIN/" "$CONFIG"
-fi
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 case "$1" in
-
-healthcheck)
-  if curl -q -LSsf -o /dev/null -s -w "200" "http://localhost/server-health"; then
-    echo "OK"
-    exit 0
-  else
-    echo "FAIL"
-    exit 10
-  fi
+--help) # Help message
+  echo 'Docker container for '$APPNAME''
+  echo "Usage: $APPNAME [healthcheck, bash, command]"
+  echo "Failed command will have exit code 10"
+  echo
+  exitCode=$?
   ;;
 
-bash | shell | sh)
-  ARGS="$*"
-  exec /bin/bash ${ARGS:--l}
+healthcheck) # Docker healthcheck
+  echo "$(uname -s) $(uname -m) is running"
+  echo _other_commands here
+  exitCode=$?
   ;;
 
-*)
-  rm -f /usr/local/apache2/logs/httpd.pid
-  [ -f "$CONFIG" ] && exec httpd -f "$CONFIG" -DFOREGROUND "$@" || exit 10
+*/bin/sh | */bin/bash | bash | shell | sh) # Launch shell
+  shift 1
+  echo "running command: $* in bash"
+  __exec_bash "${@:-/bin/bash}"
+  exitCode=$?
+  ;;
+
+*) # Execute primary command
+  echo "running command: $*"
+  __exec_bash "${@:-/bin/bash}"
+  exitCode=$?
   ;;
 esac
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end of entrypoint
+exit ${exitCode:-$?}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
